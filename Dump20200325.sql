9613022f4cb2277c70d7179df5ebcb180ef0fb0b
fresher93 <60925770+fresher93@users.noreply.github.com>
2020-03-25 21:33:15 +0700

    Push sql dump file 20200325.sql

--- Dump20200325.sql
+++ Dump20200325.sql
@@ -36,7 +36,7 @@ CREATE TABLE `users` (
   `updated_date` datetime NOT NULL,
   `updated_by` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'admin','admin','admin','https://www.facebook.com/','','ADMIN',1,'2020-02-25 20:26:12','ADMIN','2020-02-25 20:26:12','ADMIN'),(3,'user1','user1','`','https://www.facebook.com/','user1/','USER',1,'2020-02-25 20:26:12','ADMIN','2020-03-20 05:21:20','Admin'),(4,'user2','user2','`','https://www.facebook.com/','user2/','USER',1,'2020-02-25 20:26:12','ADMIN','2020-03-20 05:21:34','Admin'),(5,'user3','user3','`','https://www.facebook.com/','user3/','USER',1,'2020-03-20 02:04:47','ADMIN','2020-03-20 05:21:36','Admin'),(6,'a','a','a','a','a/','USER',1,'2020-03-20 04:39:25','ADMIN','2020-03-20 05:21:33','Admin'),(7,'b','b','b','b','b/','USER',1,'2020-03-20 05:21:45','ADMIN','2020-03-20 05:22:12','Admin'),(8,'c','c','c','https://www.facebook.com/123abc','c/','USER',1,'2020-03-20 05:23:03','ADMIN','2020-03-20 05:23:07','Admin'),(9,'d','d','d','https://www.facebook.com/125234523452345fudydydydhgvjv34523453453453fsdfv','d/','USER',1,'2020-03-20 05:23:22','ADMIN','2020-03-20 05:25:53','Admin'),(10,'e','e','e','https://www.facebook.com/125234523452345fudydydydhgvjv345234534534','e/','USER',1,'2020-03-20 05:23:48','ADMIN','2020-03-20 05:25:53','Admin');
+INSERT INTO `users` VALUES (1,'admin','admin','admin','https://www.facebook.com/','','ADMIN',1,'2020-02-25 20:26:12','ADMIN','2020-02-25 20:26:12','ADMIN'),(14,'nguoi dung 1','user1','1','http://www.facebook.com/user1','user1/','USER',1,'2020-03-25 02:23:44','ADMIN','2020-03-25 02:23:46','Admin');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-23 22:09:08
+-- Dump completed on 2020-03-25 21:32:30
